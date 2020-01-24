3e60b2a708dd0291bd0a6e71961d93f0b2a14437
qkudev <qkud.ev@gmail.com>
2020-01-24 17:31:48 +0700

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import path from 'path';
 import Telegram from 'node-telegram-bot-api';
+import Telegraf from 'telegraf';
+import express from 'express';
 
 const token1 = '901382200:AAFbnOqObG7X2Xu_pJYs_5NBDYy25wX2_LY';
 const token2 = '1097729467:AAEBDGZbIRf-Kr3hKeQp7ekEARZa_rwwK24';
 
-const bot1 = new Telegram(token1, { polling: true });
-const bot2 = new Telegram(token2, { polling: true });
+const expressApp = express();
 
-bot1.onText(/\/start/, async (msg) => {
-  const chatId = msg.chat.id;
-  await bot1.sendMessage(chatId, 'HELLO 1');
+const bot = new Telegraf(token1);
+expressApp.use(bot.webhookCallback('/secret-path'));
+bot.telegram.setWebhook('https://server.tld:8443/secret-path');
+
+bot.command('/start', async (ctx) => {
+  await ctx.reply('HELLO 1');
 })
 
-bot2.onText(/\/start/, async (msg) => {
-  const chatId = msg.chat.id;
-  await bot1.sendMessage(chatId, 'HELLO 2');
+expressApp.use('*', (req,res, next) => {
+  console.log(`${new Date().toLocaleDateString()} incoming request: `, req.path);
+  next();
 })
+
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+expressApp.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+});
